d34e2f6233ea9ce136f03290ae7b21b79ac3fb5e
conrado stella <conrado@madatech.com.br>
2026-01-20 21:47:34 -0300

    feat: initial setup, consuming new api

--- src/index.js
+++ src/index.js
@@ -9,11 +9,18 @@ let cache = null; // cache sem TTL (bug adicional)
 app.get('/pix/participants/:ispb', async (req, res) => {
   const { ispb } = req.params;
 
-  if (!cache) {
-    const response = await axios.get(process.env.BCB_PIX_URL);
-    cache = response.data;
+  if (!ispb || ispb.length !== 8 || !/^\d+$/.test(ispb)) {
+    return res.status(400).json({ error: 'Invalid ISPB code' });
   }
 
+  console.log(`${process.env.BCB_PIX_URL}?@Data=${encodeURIComponent(`'2020-11-16'`)}&$filter=${encodeURIComponent(`ISPB eq '${ispb}'`)}&$format=json`);
+
+  // if (!cache) {
+    const response = await axios.get(`${process.env.BCB_PIX_URL}?@Data=${encodeURIComponent(`'2020-11-16'`)}&$filter=${encodeURIComponent(`ISPB eq '${ispb}'`)}&$format=json`);
+    cache = response.data?.value;
+  // }
+
+  console.log(cache);
   const participant = cache.find(p => p.ispb === ispb); // BUG proposital
 
   if (!participant) {
